6ea35f6d63649a4ffdd7d518ae695c59ff30107e
Anshul Bhilare <terminatoranshul@gmail.com>
2025-09-17 18:03:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 'use strict';
 
-/**
- * navbar variables
- */
+
 
 const navOpenBtn = document.querySelector("[data-menu-open-btn]");
 const navCloseBtn = document.querySelector("[data-menu-close-btn]");
@@ -25,9 +23,6 @@ for (let i = 0; i < navElemArr.length; i++) {
 
 
 
-/**
- * header sticky
- */
 
 const header = document.querySelector("[data-header]");
 
@@ -39,9 +34,6 @@ window.addEventListener("scroll", function () {
 
 
 
-/**
- * go top
- */
 
 const goTopBtn = document.querySelector("[data-go-top]");
 
